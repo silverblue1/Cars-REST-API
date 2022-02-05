d5d6e73718fa5579254552ded10060c79356d264
silverblue1 <sutirthac97@gmail.com>
2022-02-05 16:51:52 +0530

    fixed typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const CarsRouter = require("./routes/CarsRouter.js");
 
 const app = express();
-const PORT = 8080 || process.env.PORT;
+const port = 8080 || process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -12,6 +12,6 @@ app.use(cors());
 
 app.use("/cars", CarsRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}...`);
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}...`);
 })
